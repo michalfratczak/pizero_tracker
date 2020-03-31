f46dfcac9290d58daf92c3714885e0f0d2174f07
Michal Fratczak <michal@cgarea.com>
2020-03-31 11:27:05 +0200

    wip

--- tracker/code/mtx2b/mtx2b_test.cpp
+++ tracker/code/mtx2b/mtx2b_test.cpp
@@ -8,6 +8,11 @@ int main()
 	int radio_fd = mtx2b_open("/dev/serial0", 300); //"/dev/serial0"
 	std::cout<<"radio_fd "<<radio_fd<<std::endl;
 
-	std::string msg("message on radio waves !");
-	std::cout<<mtx2b_write(radio_fd, msg)<<std::endl;
+	int count = 5;
+	while(count--)
+	{
+		std::string msg("message on radio waves !");
+		std::cout<<msg<<" ";
+		std::cout<<mtx2b_write(radio_fd, msg)<<std::endl;
+	}
 }
