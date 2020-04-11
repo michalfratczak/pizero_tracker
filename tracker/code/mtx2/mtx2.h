a8ed3c9ba03d6f46f8b95bc1f404811df6ed0e1d
Michal Fratczak <michal@cgarea.com>
2020-04-11 14:00:14 +0200

    wip

--- tracker/code/mtx2/mtx2.h
+++ tracker/code/mtx2/mtx2.h
@@ -22,8 +22,6 @@ int mtx2_open(const std::string i_device, const baud_t baud); //"/dev/serial0"
 int mtx2_write(const int serial_file_descriptor, const char* msg, const size_t msg_sz);
 int mtx2_write(const int serial_file_descriptor, const std::string& msg);
 
-// will open and close i_device
-// void mtx2_set_frequency(const std::string i_device, const float freq_Mhz); // 434.250
 void mtx2_set_frequency(const int mtx2_enable_pin, const float freq_Mhz); // 434.250
 
 int baud_t_to_int(const baud_t&);
